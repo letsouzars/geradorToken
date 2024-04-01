51d36df211b9f1c0711ec28b0b0f292a1341e31d
Leticia Souza <133662327+letsouzars@users.noreply.github.com>
2024-04-01 08:36:09 -0300

    f
    f

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native'
+import { Routes } from './routes'
 
 export default function App() {
+
   return (
-    <View style={ESTILO.container}>
-      <Text>
-        Meu app!
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   )
 }
-
-const ESTILO = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f3f3ff",
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
